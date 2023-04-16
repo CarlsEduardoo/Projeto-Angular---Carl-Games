5bfc4d6a2b9ce460f7af0ae1703e6f38164cb301
CarlsEduardoo <Carlos.eduardodk60@gmail.com>
2023-04-16 14:55:04 -0400

    Atualização do codigo

---
+++ src/app/restrito/restrito.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
+
+@Component({
+  selector: 'app-restrito',
+  templateUrl: './restrito.component.html',
+  styleUrls: ['./restrito.component.css']
+})
+export class RestritoComponent {
+
+}
