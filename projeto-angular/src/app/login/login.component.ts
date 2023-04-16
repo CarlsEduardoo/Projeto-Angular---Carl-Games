d5f557093c33dd88e3ed37d9097b2bbf1db55844
CarlsEduardoo <Carlos.eduardodk60@gmail.com>
2023-04-16 15:07:46 -0400

    Atulização pagina de login, logout e CRUD

--- projeto-angular/src/app/login/login.component.ts
+++ projeto-angular/src/app/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
 
+  usuario!: string;
+  senha!: string;
+
+  constructor(private _loginService: LoginService, private _router: Router) {}
+
+  fazerLogin() {
+    this._loginService.login(this.usuario, this.senha);
+    this._router.navigate(['/restrito/lista']);
+    this._loginService.setMostraMenu(false);
+  }
 }
